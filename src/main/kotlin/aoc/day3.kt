360695ce4a0e106d6f4cd9db48b0cfb4e703aad0
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-03 16:12:45 +0100

    Simplify day3 toScore

--- src/main/kotlin/aoc/day3.kt
+++ src/main/kotlin/aoc/day3.kt
@@ -11,4 +11,4 @@ fun String.day3b(): Int = splitToSequence("\n")
     .windowed(3, 3) { (a, b, c) -> a.toSet().intersect(b.toSet()).intersect(c.toSet()).single().toScore() }
     .sum()
 
-fun Char.toScore() = (1 + code - 'a'.code).takeIf { it > 0 } ?: (27 + code - 'A'.code)
+fun Char.toScore() = if (code > 96) code - 96 else code - 38
