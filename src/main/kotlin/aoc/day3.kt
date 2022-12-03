45c6cd5bd3f613c5451db903c06563be2640c15a
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-03 16:09:43 +0100

    Clean up day3

--- src/main/kotlin/aoc/day3.kt
+++ src/main/kotlin/aoc/day3.kt
@@ -1,19 +1,14 @@
 package aoc
 
-fun String.day3a(): Int {
-    return splitToSequence("\n").map { s ->
-        val (left, right) = s.withIndex()
-            .partition { it.index < s.length / 2 }
-        left.map { it.value }
-            .intersect(right.map { it.value }.toSet())
-            .sumOf { c -> c.toScore() }
-    }.sum()
-}
+fun String.day3a(): Int = splitToSequence("\n").map { line ->
+    val (left, right) = line.withIndex().partition { it.index < line.length / 2 }
+    left.map { it.value }
+        .intersect(right.map { it.value }.toSet())
+        .sumOf { it.toScore() }
+}.sum()
 
-fun String.day3b(): Int {
-    return splitToSequence("\n").windowed(size = 3, step = 3) { (a, b, c) ->
-        a.toSet().intersect(b.toSet()).intersect(c.toSet()).single().toScore()
-    }.sum()
-}
+fun String.day3b(): Int = splitToSequence("\n")
+    .windowed(3, 3) { (a, b, c) -> a.toSet().intersect(b.toSet()).intersect(c.toSet()).single().toScore() }
+    .sum()
 
 fun Char.toScore() = (1 + code - 'a'.code).takeIf { it > 0 } ?: (27 + code - 'A'.code)
