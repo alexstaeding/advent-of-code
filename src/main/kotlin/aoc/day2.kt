68ccbb023a24345b12f72f6f4a461faf9749d204
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-03 14:04:44 +0100

    TDD day2 b

--- src/main/kotlin/aoc/day2.kt
+++ src/main/kotlin/aoc/day2.kt
@@ -10,39 +10,26 @@ fun main() {
 
 // model
 
-data class Game(val opponent: Hand, val outcome: Char)
-enum class Hand {
-    ROCK, PAPER, SCISSORS;
-
-    companion object
-}
+data class Game(val opponent: Char, val outcome: Char)
 
 // game logic
 
-operator fun Hand.Companion.get(n: Int) = Hand.values()[(3 + n) % 3]
+fun convertChar(n: Int) = listOf('A', 'B', 'C')[(3 + n) % 3]
 
-fun Game.calculatePlayerPoints(): Int = opponent.pickHand(outcome).ordinal + 1 + (outcome.code - 'X'.code) * 3
+fun Game.calculatePlayerPoints(): Int = opponent.pickHand(outcome).code - 'A'.code + 1 + (outcome.code - 'X'.code) * 3
 
 /**
  * X = lose
  * Y = draw
  * Z = win
  */
-fun Hand.pickHand(outcome: Char): Hand = when (outcome) {
-    'X' -> Hand[ordinal - 1]
+fun Char.pickHand(outcome: Char): Char = when (outcome) {
+    'X' -> convertChar(code - 'A'.code - 1)
     'Y' -> this
-    'Z' -> Hand[ordinal + 1]
+    'Z' -> convertChar(code - 'A'.code + 1)
     else -> error("Invalid outcome: $outcome")
 }
 
 // parsing
 
-fun String.toGame() = Game(this[0].toHand(), this[2])
-
-fun Char.toHand(): Hand = when (this) {
-    'A' -> Hand.ROCK
-    'B' -> Hand.PAPER
-    'C' -> Hand.SCISSORS
-    else -> throw IllegalArgumentException("Invalid hand: $this")
-}
-
+fun String.toGame() = Game(this[0], this[2])
