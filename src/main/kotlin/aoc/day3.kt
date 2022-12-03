85f2f1aa60c48de6ac691ed16c405d274d9169de
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-03 18:00:44 +0100

    .windowed(3,3) -> .chunked(3)

--- src/main/kotlin/aoc/day3.kt
+++ src/main/kotlin/aoc/day3.kt
@@ -9,11 +9,11 @@ fun String.day3a(): Int = splitToSequence("\n").map { line ->
 
 fun String.day3b(): Int = splitToSequence("\n")
     .map { it.toSet() }
-    .windowed(3, 3) { (a, b, c) -> a.intersect(b).intersect(c).single().toScore() }
+    .chunked(3) { (a, b, c) -> a.intersect(b).intersect(c).single().toScore() }
     .sum()
 
 fun Char.toScore() = if (code > 96) code - 96 else code - 38
 
 // golfed versions of b
-// 145: fun String.b()=split("\n").map{it.toSet()}.windowed(3,3){(a,b,c)->a.intersect(b).intersect(c).first().run{if(code>96)code-96 else code-38}}.sum()
-// 157: println(java.io.File("a").readLines().map{it.toSet()}.windowed(3,3){(a,b,c)->a.intersect(b).intersect(c).first().run{if(code>96)code-96 else code-38}}.sum())
+// 142: fun String.b()=split("\n").map{it.toSet()}.chunked(3){(a,b,c)->a.intersect(b).intersect(c).first().run{if(code>96)code-96 else code-38}}.sum()
+// 154: println(java.io.File("a").readLines().map{it.toSet()}.chunked(3){(a,b,c)->a.intersect(b).intersect(c).first().run{if(code>96)code-96 else code-38}}.sum())
