7e1c2f74b69287d68239599d0808a5739afd73ca
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-03 14:07:56 +0100

    TDD day2 e

--- src/main/kotlin/aoc/day2.kt
+++ src/main/kotlin/aoc/day2.kt
@@ -10,26 +10,26 @@ fun main() {
 
 // model
 
-data class Game(val opponent: Int, val outcome: Char)
+data class Game(val opponent: Int, val outcome: Int)
 
 // game logic
 
 fun convert(n: Int) = (3 + n) % 3
 
-fun Game.calculatePlayerPoints(): Int = opponent.pickHand(outcome) + 1 + (outcome.code - 'X'.code) * 3
+fun Game.calculatePlayerPoints(): Int = opponent.pickHand(outcome) + 1 + outcome * 3
 
 /**
  * X = lose
  * Y = draw
  * Z = win
  */
-fun Int.pickHand(outcome: Char): Int = when (outcome) {
-    'X' -> convert(this - 1)
-    'Y' -> this
-    'Z' -> convert(this + 1)
+fun Int.pickHand(outcome: Int): Int = when (outcome) {
+    0 -> convert(this - 1)
+    1 -> this
+    2 -> convert(this + 1)
     else -> error("Invalid outcome: $outcome")
 }
 
 // parsing
 
-fun String.toGame() = Game(this[0].code - 'A'.code, this[2])
+fun String.toGame() = Game(this[0].code - 'A'.code, this[2].code - 'X'.code)
