5e2b61d6eeaa1c29fafe5782f0690412e63cb18b
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-03 15:48:55 +0100

    Clean up printlns

--- src/main/kotlin/aoc/day3.kt
+++ src/main/kotlin/aoc/day3.kt
@@ -4,13 +4,11 @@ fun main() = println(getInput(3).day3())
 
 fun String.day3(): Int {
     return splitToSequence("\n").map { s ->
-        println("test: $s")
         val (left, right) = s.withIndex()
             .partition { it.index < s.length / 2 }
         left.map { it.value }
             .intersect(right.map { it.value }.toSet())
             .sumOf { c -> c.toScore() }
-            .also { println("Sum: $it") }
     }.sum()
 }
 
