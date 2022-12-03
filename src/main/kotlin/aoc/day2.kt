b670189cd9f8b500b139cb0cdee0dd8bcb3a2285
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-03 18:45:07 +0100

    Clean up day2

--- src/main/kotlin/aoc/day2.kt
+++ src/main/kotlin/aoc/day2.kt
@@ -1,5 +1,5 @@
 package aoc
 
 fun String.day2(): Int = splitToSequence("\n")
-    .map { it[0].code - 'A'.code to it[2].code - 'X'.code }
-    .sumOf { (a, b) -> (3 + (a + b - 1)) % 3 + 1 + b * 3 }
+    .map { it[0].code - 65 to it[2].code - 88 }
+    .sumOf { (a, b) -> (2 + a + b) % 3 + 1 + b * 3 }
