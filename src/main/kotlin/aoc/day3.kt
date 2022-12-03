4c1614b41d57805456c9808229307369029203f6
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-03 15:48:10 +0100

    Day 3 fix for multiline

--- src/main/kotlin/aoc/day3.kt
+++ src/main/kotlin/aoc/day3.kt
@@ -3,9 +3,15 @@ package aoc
 fun main() = println(getInput(3).day3())
 
 fun String.day3(): Int {
-    return withIndex().partition { it.index < length / 2 }.let { (left, right) ->
-        left.map { it.value }.intersect(right.map { it.value }.toSet()).sumOf { c ->
-            (c.code - ('a'.code - 1)).takeIf { it > 0 } ?: (c.code - ('A'.code - 1))
-        }
-    }
+    return splitToSequence("\n").map { s ->
+        println("test: $s")
+        val (left, right) = s.withIndex()
+            .partition { it.index < s.length / 2 }
+        left.map { it.value }
+            .intersect(right.map { it.value }.toSet())
+            .sumOf { c -> c.toScore() }
+            .also { println("Sum: $it") }
+    }.sum()
 }
+
+fun Char.toScore() = (1 + code - 'a'.code).takeIf { it > 0 } ?: (27 + code - 'A'.code)
--- src/test/kotlin/aoc/Day3Test.kt
+++ src/test/kotlin/aoc/Day3Test.kt
@@ -4,14 +4,44 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 class Day3Test : FunSpec({
+
+    test("toScore") {
+        'a'.toScore() shouldBe 1
+        'A'.toScore() shouldBe 1 + 26
+        'b'.toScore() shouldBe 2
+        'B'.toScore() shouldBe 2 + 26
+        'p'.toScore() shouldBe 16
+        'P'.toScore() shouldBe 16 + 26
+        'z'.toScore() shouldBe 26
+        'Z'.toScore() shouldBe 26 + 26
+    }
+
     test("testVerySimpleA") {
         """
             vJrwpWtwJgWrhcsFMMfFFhFp
-        """.trimIndent().day3() shouldBe 'p'.code - ('a'.code - 1)
+        """.trimIndent().day3() shouldBe 'p'.toScore()
     }
     test("testVerySimpleB") {
         """
             jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-        """.trimIndent().day3() shouldBe 'L'.code - ('A'.code - 1)
+        """.trimIndent().day3() shouldBe 'L'.toScore()
+    }
+    test("testBasicMultiLine") {
+        """
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+        """.trimIndent().trimEnd().day3() shouldBe sequenceOf(
+            'p',
+            'L',
+            'P',
+            'v',
+            't',
+            's',
+        ).sumOf { it.toScore() }
+
     }
 })
