50fc60b3715154870a52c8a154e4e75f6e722891
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-03 13:17:39 +0100

    Day 2b

--- src/main/kotlin/aoc/day2.kt
+++ src/main/kotlin/aoc/day2.kt
@@ -10,34 +10,48 @@ fun main() {
 
 // model
 
-data class Game(val opponent: Hand, val player: Hand)
-enum class Hand { ROCK, PAPER, SCISSORS }
+data class Game(val opponent: Hand, val outcome: Outcome)
+enum class Hand {
+    ROCK, PAPER, SCISSORS;
+
+    companion object
+}
+
+enum class Outcome { LOSE, DRAW, WIN }
 
 // game logic
 
-fun Game.calculatePlayerPoints(): Int = when {
-    opponent.beats(player) -> 0
-    player.beats(opponent) -> 6
-    else -> 3
-} + player.ordinal + 1
+operator fun Hand.Companion.get(n: Int) = Hand.values()[(3 + n) % 3]
 
-fun Hand.beats(other: Hand): Boolean = when (this) {
-    Hand.ROCK -> other == Hand.SCISSORS
-    Hand.PAPER -> other == Hand.ROCK
-    Hand.SCISSORS -> other == Hand.PAPER
+fun Game.calculatePlayerPoints(): Int = opponent.pickHand(outcome).ordinal + 1 + outcome.ordinal * 3
+
+/**
+ * X = lose
+ * Y = draw
+ * Z = win
+ */
+fun Hand.pickHand(outcome: Outcome): Hand = when (outcome) {
+    Outcome.LOSE -> Hand[ordinal - 1]
+    Outcome.DRAW -> this
+    Outcome.WIN -> Hand[ordinal + 1]
+    else -> error("Invalid outcome: $outcome")
 }
 
 // parsing
 
-fun String.toGame(): Game {
-    val (opponent, player) = split(" ")
-    return Game(opponent.toHand(), player.toHand())
+fun String.toGame() = Game(this[0].toHand(), this[2].toOutcome())
+
+fun Char.toHand(): Hand = when (this) {
+    'A' -> Hand.ROCK
+    'B' -> Hand.PAPER
+    'C' -> Hand.SCISSORS
+    else -> throw IllegalArgumentException("Invalid hand: $this")
 }
 
-fun String.toHand(): Hand = when (this) {
-    "A", "X" -> Hand.ROCK
-    "B", "Y" -> Hand.PAPER
-    "C", "Z" -> Hand.SCISSORS
-    else -> throw IllegalArgumentException("Invalid hand: $this")
+fun Char.toOutcome(): Outcome = when (this) {
+    'X' -> Outcome.LOSE
+    'Y' -> Outcome.DRAW
+    'Z' -> Outcome.WIN
+    else -> throw IllegalArgumentException("Invalid outcome: $this")
 }
 
