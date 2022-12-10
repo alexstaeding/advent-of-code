fb6d3ad17773e987f5bb20af7cc832d4ed393d53
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-10 17:47:42 +0100

    Day 10b

--- src/main/kotlin/aoc/day10.kt
+++ src/main/kotlin/aoc/day10.kt
@@ -25,8 +25,51 @@ fun String.day10a(): Int {
     return strength
 }
 
+fun String.day10b() {
+    var x = 1
+    var ctr = 0
+    lineSequence().forEach { line ->
+        if (line.startsWith("noop")) {
+            printPixel(ctr, x)
+            ++ctr
+            if (ctr % 40 == 0) {
+                println()
+            }
+            return@forEach
+        }
+        val (op, arg) = line.split(" ")
+        when (op) {
+            "addx" -> {
+                printPixel(ctr, x)
+                ++ctr
+                if (ctr % 40 == 0) {
+                    println()
+                }
+                printPixel(ctr, x)
+                ++ctr
+                if (ctr % 40 == 0) {
+                    println()
+                }
+                x += arg.toInt()
+            }
+
+            else -> error("unknown op $op")
+        }
+    }
+}
+
 fun getSignalStrength(ctr: Int, x: Int): Int {
-    return if ((ctr+20) % 40 == 0) {
+    return if ((ctr + 20) % 40 == 0) {
         ctr * x
     } else 0
+}
+
+fun printPixel(ctr: Int, x: Int) {
+    // get pixel position in row
+    val pixel = ctr % 40
+    if (pixel in setOf(-1, 0, 1).map { it + x }) {
+        print("#")
+    } else {
+        print(".")
+    }
 }
