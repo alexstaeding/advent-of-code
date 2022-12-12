38f32b668821df7bf6b57b78b4d571f284601d99
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-12 19:12:26 +0100

    Day12b finished

--- src/main/kotlin/aoc/day12.kt
+++ src/main/kotlin/aoc/day12.kt
@@ -3,23 +3,20 @@ package aoc
 import java.util.PriorityQueue
 import kotlin.math.abs
 
-fun main() {
-    println(getInput(12).day12a())
-}
-
 fun String.day12a(): Int {
     val state = readState()
-    val startPos = state.find(0)
-    state.print()
-    println("startPos = $startPos")
     val steps = state.findGoal()
     checkNotNull(steps) { "No solution" }
-    println(steps.withIndex().joinToString("\n") { (i, step) -> "$i: $step" })
     return steps.size
 }
 
-var counter = 0
-
+fun String.day12b(): Int {
+    val state = readState()
+    return state.findAll { it.height == 0 || it.height == 1 }
+        .map { state.copy(start = it).also { s -> s.heightmap.reset() } }
+        .mapNotNull { it.findGoal() }
+        .minOf { it.size }
+}
 
 // A star search
 private fun State12.findGoal(): List<Pos12>? {
@@ -27,14 +24,13 @@ private fun State12.findGoal(): List<Pos12>? {
     queue.add(this[start] ?: return null)
     while (queue.isNotEmpty()) {
         val node = queue.poll()
-        println("node = $node")
         if (node.pos == goal) {
             return node.path
         }
         for (dir in Dir12.values()) {
             val newNode = this[node.pos.moveOne(dir)] ?: continue
             if (newNode.height > node.height + 1) continue
-            if (newNode.pathSize > node.pathSize + 1) {
+            if (newNode.pathSize > node.pathSize + 1 || node.pos == start) {
                 newNode.path.clear()
                 newNode.path.addAll(node.path)
                 newNode.path.add(newNode.pos)
@@ -51,8 +47,9 @@ private data class Pos12(val y: Int, val x: Int) {
 
 private enum class Dir12 { N, E, S, W }
 
+private typealias HeightMap = List<List<Node12>>
 private data class State12(
-    val heightmap: List<List<Node12>>,
+    val heightmap: HeightMap,
     val start: Pos12,
     val goal: Pos12,
 )
@@ -62,10 +59,9 @@ private data class Node12(
     val height: Int,
     val distance: Int,
     val path: MutableList<Pos12> = mutableListOf(),
-    val isStart: Boolean = false,
 ) {
     val pathSize: Int
-        get() = if (path.isEmpty() && !isStart) Int.MAX_VALUE shr 1 else path.size
+        get() = if (path.isEmpty()) Int.MAX_VALUE shr 1 else path.size
 }
 
 private fun State12.print() {
@@ -74,14 +70,16 @@ private fun State12.print() {
     }
 }
 
+private fun HeightMap.reset() = forEach { y -> y.forEach { it.path.clear() } }
+
 private operator fun State12.get(pos: Pos12): Node12? = heightmap.getOrNull(pos.y)?.getOrNull(pos.x)
 
-private fun State12.find(height: Int): Pos12 {
-    return heightmap.mapIndexedNotNull { y, nodes ->
-        nodes.indexOfFirst { it.height == height }
+private fun State12.findAll(pred: (Node12) -> Boolean): Sequence<Pos12> {
+    return heightmap.asSequence().mapIndexedNotNull { y, nodes ->
+        nodes.indexOfFirst(pred)
             .takeIf { it >= 0 }
             ?.let { y to it }
-    }.first().toPos()
+    }.map { it.toPos() }
 }
 
 private fun Pos12.moveOne(dir: Dir12) = when (dir) {
@@ -109,6 +107,7 @@ private fun String.readState(): State12 {
                     startPos = Pos12(y, x)
                     0
                 }
+
                 'E' -> {
                     endPos = Pos12(y, x)
                     27
@@ -123,7 +122,7 @@ private fun String.readState(): State12 {
     val heightMap = heights.mapIndexed { y, line ->
         line.mapIndexed { x, h ->
             val pos = Pos12(y, x)
-            Node12(pos, h, pos.distanceTo(goal), isStart = pos == start)
+            Node12(pos, h, pos.distanceTo(goal))
         }
     }
     return State12(heightMap, start, goal)
--- src/test/kotlin/aoc/Day12Test.kt
+++ src/test/kotlin/aoc/Day12Test.kt
@@ -13,4 +13,19 @@ class Day12Test : FunSpec({
             abdefghi
         """.trimIndent().day12a() shouldBe 31
     }
+    test("inputFileA") {
+        getInput(12).day12a() shouldBe 394
+    }
+    test("basicExampleB") {
+        """
+            Sabqponm
+            abcryxxl
+            accszExk
+            acctuvwj
+            abdefghi
+        """.trimIndent().day12b() shouldBe 29
+    }
+    test("inputFileB") {
+        getInput(12).day12b() shouldBe 388
+    }
 })
