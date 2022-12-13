1f6f672d3e28f4928449c15c2d5108a47ca8e0c9
Alexander Städing <alexanderstaeding@gmail.com>
2022-12-13 08:01:37 +0100

    Day13 part b

--- src/main/kotlin/aoc/day13.kt
+++ src/main/kotlin/aoc/day13.kt
@@ -1,24 +1,32 @@
 package aoc
 
+fun main() {
+    getInput(13).day13().forEach {
+        println(it)
+    }
+}
+
+// list of indices of pairs that are in the right order
+fun String.day13(): List<Int> {
+    return split("\n\n").mapIndexedNotNull { id, group ->
+        val (first, second) = group.lines().map { it.parseBrackets() }
+        if (first < second) id + 1 else null
+    }
+}
+
 fun String.parseBrackets(position: Int): Pair<ListElement, Int> {
     val list = mutableListOf<ListElement>()
     var i = position
     while (i < length) {
-        when (val c = get(i)) {
+        when (get(i)) {
             '[' -> {
                 val (subList, nextI) = parseBrackets(i + 1)
                 list.add(subList)
                 i = nextI
             }
-
             ']' -> return SubList(list) to i
-            ',' -> {
-                // do nothing
-            }
-
-            else -> {
-                list.add(Value(c.toString().toInt()))
-            }
+            ',' -> Unit
+            else -> list.add(Value(substring(i).substringBefore(',').substringBefore(']').toInt()))
         }
         ++i
     }
@@ -27,11 +35,33 @@ fun String.parseBrackets(position: Int): Pair<ListElement, Int> {
 
 fun String.parseBrackets(): ListElement = parseBrackets(0).first
 
-sealed interface ListElement
+sealed interface ListElement : Comparable<ListElement>
 data class SubList(val elements: List<ListElement>) : ListElement {
     override fun toString(): String = elements.joinToString(", ", "[", "]")
+    override fun compareTo(other: ListElement): Int {
+        val result = when (other) {
+            is SubList -> {
+                elements
+                    .zip(other.elements) { left, right -> left.compareTo(right) }
+                    .firstOrNull { it != 0 }
+                    ?: elements.size.compareTo(other.elements.size)
+            }
+
+            is Value -> compareTo(SubList(listOf(other)))
+        }
+        println("compared $this ${if (result < 0) "<" else if (result == 0) "==" else ">"} $other")
+        return result
+    }
 }
 
 data class Value(val value: Int) : ListElement {
     override fun toString(): String = value.toString()
+    override fun compareTo(other: ListElement): Int {
+        val result = when (other) {
+            is SubList -> SubList(listOf(this)).compareTo(other)
+            is Value -> value.compareTo(other.value)
+        }
+        println("compared $this ${if (result < 0) "<" else if (result == 0) "==" else ">"} $other")
+        return result
+    }
 }
--- src/test/kotlin/aoc/Day13Test.kt
+++ src/test/kotlin/aoc/Day13Test.kt
@@ -1,6 +1,8 @@
 package aoc
 
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.comparables.shouldBeGreaterThan
+import io.kotest.matchers.comparables.shouldBeLessThan
 import io.kotest.matchers.shouldBe
 
 class Day13Test : FunSpec({
@@ -51,5 +53,79 @@ class Day13Test : FunSpec({
                     Value(9),
                 )
             )
+    }
+    test("compareBasic1") {
+        "[1,1,3,1,1]".parseBrackets() shouldBeLessThan "[1,1,5,1,1]".parseBrackets()
+    }
+    test("compareBasic2") {
+        "[[1],[2,3,4]]".parseBrackets() shouldBeLessThan "[[1],4]".parseBrackets()
+    }
+    test("compareBasic3") {
+        "[9]".parseBrackets() shouldBeGreaterThan "[[8,7,6]]".parseBrackets()
+    }
+    test("compareBasic4") {
+        "[[4,4],4,4]".parseBrackets() shouldBeLessThan "[[4,4],4,4,4]".parseBrackets()
+    }
+    test("compareBasic5") {
+        "[7,7,7,7]".parseBrackets() shouldBeGreaterThan "[7,7,7]".parseBrackets()
+    }
+    test("compareBasic6") {
+        "[]".parseBrackets() shouldBeLessThan "[3]".parseBrackets()
+    }
+    test("compareBasic7") {
+        "[[[]]]".parseBrackets() shouldBeGreaterThan "[[]]".parseBrackets()
+    }
+    test("compareBasic8") {
+        "[1,[2,[3,[4,[5,6,7]]]],8,9]".parseBrackets() shouldBeGreaterThan "[1,[2,[3,[4,[5,6,0]]]],8,9]".parseBrackets()
+    }
+    test("basicExampleA") {
+        """
+            [1,1,3,1,1]
+            [1,1,5,1,1]
+
+            [[1],[2,3,4]]
+            [[1],4]
+
+            [9]
+            [[8,7,6]]
+
+            [[4,4],4,4]
+            [[4,4],4,4,4]
+
+            [7,7,7,7]
+            [7,7,7]
+
+            []
+            [3]
+
+            [[[]]]
+            [[]]
+
+            [1,[2,[3,[4,[5,6,7]]]],8,9]
+            [1,[2,[3,[4,[5,6,0]]]],8,9]
+        """.trimIndent().day13() shouldBe listOf(1, 2, 4, 6)
+    }
+    test("advanced1") {
+        "[[9,2],[],[10,[6,1],5,4]]".parseBrackets() shouldBeLessThan
+            "[[[9,[],3],[6],1],[[1],[[9,1],0,7,10],2,0]]".parseBrackets()
+    }
+    test("advanced2") {
+        "[[[[9,0,4,8,4],[10,6],[9,9],[1,2]],2,2],[10,[[10,9,5,4,5]],[[10,7,1],[]],[[2,1,6],[2,6],0],10],[0,1,[2,4,[1,0,10],[4,1,1,8,5],2],10]]".parseBrackets() shouldBeGreaterThan
+            "[[[6,6],8,6],[3,[0,10],9,9],[6,3,7,2]]".parseBrackets()
+    }
+    test("advanced3") {
+        "[[],[[7,[5],3],1],[6,[],9,[7,[9,5,4,3,4]]]]".parseBrackets() shouldBeLessThan
+            "[[2,[9,[3,1,6],[],1,[7,4,7,4,0]],7,4],[[[5,0]],[[1,2],[2,10],[]],[5,[],[8,0,0,1,6]],[[7,0],[0,2,7,9]]]]".parseBrackets()
+    }
+    test("advanced4") {
+        "[[10,[[10,2],2],3,2],[5,7,[2,[9,4],0,[4,7],0]],[[6,6,10,6],[[7,5],[1,2,9,9,8],4,[9,7]],6],[8,[],8,8,[[4,6,7,7]]],[5,[7],[3,8],[0]]]".parseBrackets() shouldBeGreaterThan
+            "[[[6,1,[]]],[8,[[6,10,0,4],2,10,[0,3,10,4],9],7,7]]".parseBrackets()
+    }
+    test("advanced5") {
+        "[[10,4,[3,10,[7,6,10,4,8],3,[7]],4],[5]]".parseBrackets() shouldBeGreaterThan
+            "[[4,[]],[[[8,2,4,6,10]],2,[7,[3],[]]],[10,10,2,[2],4],[],[[2,9,[10,2,8]],4]]".parseBrackets()
+    }
+    test("inputFileA") {
+        getInput(13).day13().sum() shouldBe 5659
     }
 })
