8a57ad212772cb881dced20967c492cf040d8688
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-01 13:09:10 +0100

    Even shorter

--- years/2023/src/main/kotlin/aoc/Day1.kt
+++ years/2023/src/main/kotlin/aoc/Day1.kt
@@ -2,42 +2,30 @@ package aoc
 
 fun Sequence<String>.day1a(): Int = map { line -> "${line.first { it.isDigit() }}${line.last { it.isDigit() }}".toInt() }.sum()
 
-private val replacements = mapOf(
-    "nine" to "9",
-    "eight" to "8",
-    "seven" to "7",
-    "six" to "6",
-    "five" to "5",
-    "four" to "4",
-    "three" to "3",
-    "two" to "2",
-    "one" to "1",
-)
+private val replacements = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    .withIndex().associate { (i, s) -> s to (i + 1).toString() }
 
-private interface Matcher {
+private fun interface Matcher {
     fun read(c: Char): String?
-}
 
-private class PatternMatcher(private val pattern: String, private val result: String) : Matcher {
-    private var matches: Int = 0
+    object Digit : Matcher by Matcher({ c -> if (c.isDigit()) c.toString() else null })
+    class Pattern(private val pattern: String, private val result: String) : Matcher {
+        private var matches: Int = 0
 
-    override fun read(c: Char): String? {
-        matches = when (c) {
-            pattern[matches] -> matches + 1
-            pattern[0] -> 1
-            else -> 0
+        override fun read(c: Char): String? {
+            matches = when (c) {
+                pattern[matches] -> matches + 1
+                pattern[0] -> 1
+                else -> 0
+            }
+            return result.takeIf { matches == pattern.length }
         }
-        return result.takeIf { matches == pattern.length }
     }
 }
 
-private object DigitMatcher : Matcher {
-    override fun read(c: Char): String? = if (c.isDigit()) c.toString() else null
+fun String.seek(reversed: Boolean = false): String {
+    val matchers = replacements.map { (k, v) -> Matcher.Pattern(if (reversed) k.reversed() else k, v) } + Matcher.Digit
+    return (if (reversed) reversed() else this).firstNotNullOf { c -> matchers.firstNotNullOfOrNull { it.read(c) } }
 }
 
-private fun String.seek(reversed: Boolean = false): String {
-    val matchers = replacements.map { (k, v) -> PatternMatcher(if (reversed) k.reversed() else k, v) } + DigitMatcher
-    return firstNotNullOf { c -> matchers.firstNotNullOfOrNull { it.read(c) } }
-}
-
-fun Sequence<String>.day1b(): Int = map { "${it.seek()}${it.reversed().seek(true)}".toInt() }.sum()
+fun Sequence<String>.day1b(): Int = map { "${it.seek()}${it.seek(true)}".toInt() }.sum()
