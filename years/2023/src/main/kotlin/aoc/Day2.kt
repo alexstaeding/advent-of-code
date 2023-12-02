7f1dcee07c117d9d5ce1ad45e5f89d24c4163e66
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-02 06:48:23 +0100

    2023/Day2 solve part 2

--- years/2023/src/main/kotlin/aoc/Day2.kt
+++ years/2023/src/main/kotlin/aoc/Day2.kt
@@ -1,5 +1,8 @@
 package aoc
 
+import kotlin.math.max
+import kotlin.math.min
+
 fun main() {
 //    println("Result: " + Framework.getInput(2, "a", useExample = true).readText().day2a())
     println("Result: " + Framework.getInput(2).readText().day2a())
@@ -13,6 +16,7 @@ val state = mutableMapOf(
 
 fun String.day2a(): Int {
     var possible = 0
+    var power = 0
     split("\n")
         .asSequence()
         .map { it.substringAfter(": ") }
@@ -20,25 +24,23 @@ fun String.day2a(): Int {
         .mapIndexed { index, strings -> index + 1 to strings.map { it.split(", ").map { x -> x.split(" ").zipWithNext().single() } } }
         .toList()
         .forEach { (index, subGame: List<List<Pair<String, String>>>) ->
-            var stateCopy = state.toMutableMap()
+            val stateCopy = state.toMutableMap()
             println(stateCopy)
             var f = true
+            val maxMap = mutableMapOf(
+                "red" to 0,
+                "green" to 0,
+                "blue" to 0,
+            )
             for (take in subGame) {
                 for ((num, color) in take) {
-                    stateCopy.computeIfPresent(color) { _, v -> v - num.toInt() }
+                    stateCopy.computeIfPresent(color) { _, v ->
+                        maxMap[color] = max(maxMap[color]!!, num.toInt())
+                        v - num.toInt()
+                    }
                 }
-                if (!stateCopy.all { (_, v) -> v >= 0 }) {
-                    f = false
-                    break
-                }
-                stateCopy = state.toMutableMap()
             }
-            if (f) {
-                possible += index
-            }
-            println(stateCopy)
-
-            // reset
+            power += maxMap.asSequence().fold(1) { acc, (_, v) -> acc * max(0, v) }
         }
-    return possible
+    return power
 }
