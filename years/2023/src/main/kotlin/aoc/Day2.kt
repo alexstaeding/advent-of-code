e72a87b35472e0e9249263bf41b094bb21324241
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-02 10:54:12 +0100

    Make it smol

--- years/2023/src/main/kotlin/aoc/Day2.kt
+++ years/2023/src/main/kotlin/aoc/Day2.kt
@@ -2,21 +2,19 @@ package aoc
 
 import kotlin.math.max
 
-val initialState = mutableMapOf("red" to 12, "green" to 13, "blue" to 14)
+private val initialState = mutableMapOf("red" to 12, "green" to 13, "blue" to 14)
 
-fun Sequence<String>.day2a(): Int = withIndex().sumOf { (index, line) ->
-    (index + 1).takeIf { _ ->
-        line.split(": ")[1].split("; ").map { it.split(", ").map { x -> x.split(" ") } }.all { takes ->
-            val state = initialState.toMutableMap()
-            takes.forEach { (num, color) -> state.computeIfPresent(color) { _, v -> v - num.toInt() } }
-            state.values.all { it >= 0 }
-        }
-    } ?: 0
-}
-
-fun Sequence<String>.day2b(): Int = map { line -> line.substringAfter(": ").split(", ", "; ").map { it.split(" ") } }
-    .sumOf { takes ->
-        val minMap = initialState.mapValuesTo(mutableMapOf()) { 0 }
-        takes.forEach { (num, color) -> minMap.computeIfPresent(color) { _, v -> max(v, num.toInt()) } }
-        minMap.values.reduce { a, v -> a * v }
+fun Sequence<String>.day2a(): Int = withIndex().filter { (_, line) ->
+    line.split(": ")[1].split("; ").map { it.split(", ").map { x -> x.split(" ") } }.all { takes ->
+        val state = initialState.toMutableMap()
+        takes.forEach { (num, color) -> state.computeIfPresent(color) { _, v -> v - num.toInt() } }
+        state.values.all { it >= 0 }
     }
+}.sumOf { it.index + 1 }
+
+fun Sequence<String>.day2b(): Int = sumOf { line ->
+    val minMap = initialState.mapValuesTo(mutableMapOf()) { 0 }
+    line.split(": ")[1].split("; ", ", ").map { it.split(" ") }
+        .forEach { (num, color) -> minMap.computeIfPresent(color) { _, v -> max(v, num.toInt()) } }
+    minMap.values.reduce { a, v -> a * v }
+}
