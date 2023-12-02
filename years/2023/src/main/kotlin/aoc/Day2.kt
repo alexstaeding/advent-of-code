3bb60654e43f8233920c60ba1556ca62b6e636a8
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-02 07:15:56 +0100

    Convert 2023/Day2 to expression body and clean up a bit

--- years/2023/src/main/kotlin/aoc/Day2.kt
+++ years/2023/src/main/kotlin/aoc/Day2.kt
@@ -8,43 +8,35 @@ val state = mutableMapOf(
     "blue" to 14,
 )
 
-fun String.day2a(): Int {
-    return split("\n")
-        .asSequence()
-        .map { it.substringAfter(": ") }
-        .map { it.split("; ") }
-        .mapIndexed { index, strings -> index + 1 to strings.map { it.split(", ").map { x -> x.split(" ").zipWithNext().single() } } }
-        .toList()
-        .sumOf { (index, subGame) ->
-            if (subGame.all { take ->
-                    val stateCopy = state.toMutableMap()
-                    for ((num, color) in take) {
-                        stateCopy.computeIfPresent(color) { _, v -> v - num.toInt() }
-                    }
-                    stateCopy.all { (_, v) -> v >= 0 }
-                }) {
-                index
-            } else 0
-        }
-}
-
-fun String.day2b(): Int {
-    return split("\n")
-        .asSequence()
-        .map { it.substringAfter(": ") }
-        .map { it.split("; ") }
-        .map { strings -> strings.map { it.split(", ").map { x -> x.split(" ").zipWithNext().single() } } }
-        .sumOf { subGame ->
-            val maxMap = mutableMapOf(
-                "red" to 0,
-                "green" to 0,
-                "blue" to 0,
-            )
-            for (take in subGame) {
-                for ((num, color) in take) {
-                    maxMap.computeIfPresent(color) { _, v -> max(v, num.toInt()) }
+fun String.day2a(): Int = splitToSequence("\n")
+    .map { it.substringAfter(": ").split("; ") }
+    .mapIndexed { index, strings -> index + 1 to strings.map { it.split(", ").map { x -> x.split(" ").zipWithNext().single() } } }
+    .toList()
+    .sumOf { (index, subGame) ->
+        index.takeIf {
+            subGame.all { takes ->
+                val stateCopy = state.toMutableMap()
+                for ((num, color) in takes) {
+                    stateCopy.computeIfPresent(color) { _, v -> v - num.toInt() }
                 }
+                stateCopy.all { (_, v) -> v >= 0 }
             }
-            maxMap.values.reduce { a, v -> a * v }
+        } ?: 0
+    }
+
+fun String.day2b(): Int = splitToSequence("\n")
+    .map { it.substringAfter(": ").split("; ") }
+    .map { strings -> strings.map { it.split(", ").map { x -> x.split(" ").zipWithNext().single() } } }
+    .sumOf { subGame ->
+        val maxMap = mutableMapOf(
+            "red" to 0,
+            "green" to 0,
+            "blue" to 0,
+        )
+        for (takes in subGame) {
+            for ((num, color) in takes) {
+                maxMap.computeIfPresent(color) { _, v -> max(v, num.toInt()) }
+            }
         }
-}
+        maxMap.values.reduce { a, v -> a * v }
+    }
