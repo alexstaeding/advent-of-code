f3a311f6980ad9a7240818b17dd23a24c6ea801a
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-02 09:55:30 +0100

    Make 2023/Day2 even more unreadable

--- years/2023/src/main/kotlin/aoc/Day2.kt
+++ years/2023/src/main/kotlin/aoc/Day2.kt
@@ -4,19 +4,17 @@ import kotlin.math.max
 
 val initialState = mutableMapOf("red" to 12, "green" to 13, "blue" to 14)
 
-fun Sequence<String>.day2a(): Int = map { it.substringAfter(": ").split("; ") }
-    .mapIndexed { index, strings -> index + 1 to strings.map { it.split(", ").map { x -> x.split(" ").zipWithNext().single() } } }
-    .sumOf { (index, subGame) ->
-        index.takeIf { _ ->
-            subGame.all { takes ->
-                val state = initialState.toMutableMap()
-                takes.forEach { (num, color) -> state.computeIfPresent(color) { _, v -> v - num.toInt() } }
-                state.all { (_, v) -> v >= 0 }
-            }
-        } ?: 0
-    }
+fun Sequence<String>.day2a(): Int = withIndex().sumOf { (index, line) ->
+    (index + 1).takeIf { _ ->
+        line.split(": ")[1].split("; ").map { it.split(", ").map { x -> x.split(" ") } }.all { takes ->
+            val state = initialState.toMutableMap()
+            takes.forEach { (num, color) -> state.computeIfPresent(color) { _, v -> v - num.toInt() } }
+            state.values.all { it >= 0 }
+        }
+    } ?: 0
+}
 
-fun Sequence<String>.day2b(): Int = map { line -> line.substringAfter(": ").split(", ", "; ").map { it.split(" ").zipWithNext().single() } }
+fun Sequence<String>.day2b(): Int = map { line -> line.substringAfter(": ").split(", ", "; ").map { it.split(" ") } }
     .sumOf { takes ->
         val minMap = initialState.mapValuesTo(mutableMapOf()) { 0 }
         takes.forEach { (num, color) -> minMap.computeIfPresent(color) { _, v -> max(v, num.toInt()) } }
