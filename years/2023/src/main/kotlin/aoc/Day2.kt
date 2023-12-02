80bb68182d719dbc70a9206ae614330dad062dda
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-02 07:24:39 +0100

    Make 2023/Day2 more unreadable

--- years/2023/src/main/kotlin/aoc/Day2.kt
+++ years/2023/src/main/kotlin/aoc/Day2.kt
@@ -2,31 +2,25 @@ package aoc
 
 import kotlin.math.max
 
-val state = mutableMapOf(
-    "red" to 12,
-    "green" to 13,
-    "blue" to 14,
-)
+val state = mutableMapOf("red" to 12, "green" to 13, "blue" to 14)
 
 fun Sequence<String>.day2a(): Int = map { it.substringAfter(": ").split("; ") }
     .mapIndexed { index, strings -> index + 1 to strings.map { it.split(", ").map { x -> x.split(" ").zipWithNext().single() } } }
     .sumOf { (index, subGame) ->
-        index.takeIf {
+        index.takeIf { _ ->
             subGame.all { takes ->
-                val stateCopy = state.toMutableMap()
-                for ((num, color) in takes) {
-                    stateCopy.computeIfPresent(color) { _, v -> v - num.toInt() }
+                state.toMutableMap().let {
+                    takes.forEach { (num, color) -> it.computeIfPresent(color) { _, v -> v - num.toInt() } }
+                    it.all { (_, v) -> v >= 0 }
                 }
-                stateCopy.all { (_, v) -> v >= 0 }
             }
         } ?: 0
     }
 
 fun Sequence<String>.day2b(): Int = map { line -> line.substringAfter(": ").split(", ", "; ").map { it.split(" ").zipWithNext().single() } }
     .sumOf { takes ->
-        val maxMap = state.mapValuesTo(mutableMapOf()) { 0 }
-        for ((num, color) in takes) {
-            maxMap.computeIfPresent(color) { _, v -> max(v, num.toInt()) }
+        state.mapValuesTo(mutableMapOf()) { 0 }.let {
+            takes.forEach { (num, color) -> it.computeIfPresent(color) { _, v -> max(v, num.toInt()) } }
+            it.values.reduce { a, v -> a * v }
         }
-        maxMap.values.reduce { a, v -> a * v }
     }
