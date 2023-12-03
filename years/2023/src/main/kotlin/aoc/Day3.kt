3398c0a8250e4ea61b2508f7e61c5b39c59d8532
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-03 07:26:46 +0100

    Add 2023/Day3 part b

--- years/2023/src/main/kotlin/aoc/Day3.kt
+++ years/2023/src/main/kotlin/aoc/Day3.kt
@@ -1,6 +1,6 @@
 package aoc
 
-fun main() = Framework.getInput(3, useExample = false).readText().day3a().let { println(it) }
+fun main() = Framework.getInput(3, useExample = false).readText().day3b().let { println(it) }
 
 typealias Grid = Array<CharArray>
 
@@ -8,16 +8,19 @@ private data class Pos(val y: Int, val x: Int)
 
 private operator fun Pos.plus(other: Pos) = Pos(y + other.y, x + other.x)
 
-private fun Grid.checkSurround(point: Pos): Boolean {
+private data class GearCandidate(val num: Int, val symbolPos: Set<Pos>)
+
+private fun Grid.checkSurround(point: Pos): Set<Pos> {
+    val symbols = mutableSetOf<Pos>()
     (-1..1).forEach { y ->
         (-1..1).forEach { x ->
             if (y != 0 || x != 0) {
                 val pos = point + Pos(x, y)
-                if (getOrNull(pos.y)?.getOrNull(pos.x)?.let { it != '.' && !it.isDigit() } == true) return true
+                if (getOrNull(pos.y)?.getOrNull(pos.x)?.let { it != '.' && !it.isDigit() } == true) symbols += pos
             }
         }
     }
-    return false
+    return symbols
 }
 
 fun String.day3a(): Int {
@@ -31,19 +34,14 @@ fun String.day3a(): Int {
         for ((x, c) in row.withIndex()) {
             if (!c.isDigit()) {
                 if (numHasStar) {
-                    println("Adding number $lastNum at $x, $y")
                     total += lastNum
                 }
                 numHasStar = false
             }
             if (c.isDigit()) {
                 val pos = Pos(y, x)
-                if (y == 4) {
-                    println("Checking $pos: $lastNum = ${lastNum * 10 + c.digitToInt()}")
-                }
                 lastNum = lastNum * 10 + c.digitToInt()
-                if (grid.checkSurround(pos)) {
-//                    println("Found symbol near $x, $y ($c)")
+                if (grid.checkSurround(pos).isNotEmpty()) {
                     numHasStar = true
                 }
             } else {
@@ -54,5 +52,49 @@ fun String.day3a(): Int {
             total += lastNum
         }
         total
+    }
+}
+
+fun String.day3b(): Int {
+    val grid = lines().map { it.toCharArray() }.toTypedArray()
+
+    val allGearSymbols = mutableSetOf<Pos>()
+    val candidates = mutableListOf<GearCandidate>()
+
+    grid.withIndex().forEach { (y, row) ->
+        var lastNum = 0
+        val lastSymbolPos = mutableSetOf<Pos>()
+        for ((x, c) in row.withIndex()) {
+            if (!c.isDigit()) {
+                if (lastSymbolPos.isNotEmpty()) {
+                    candidates.add(GearCandidate(lastNum, lastSymbolPos.toSet()))
+                }
+                allGearSymbols += lastSymbolPos
+                lastSymbolPos.clear()
+            }
+            if (c.isDigit()) {
+                val pos = Pos(y, x)
+                lastNum = lastNum * 10 + c.digitToInt()
+                lastSymbolPos += grid.checkSurround(pos)
+            } else {
+                lastNum = 0
+                allGearSymbols += lastSymbolPos
+                lastSymbolPos.clear()
+            }
+        }
+        if (lastSymbolPos.isNotEmpty()) {
+            candidates.add(GearCandidate(lastNum, lastSymbolPos))
+        }
+        allGearSymbols += lastSymbolPos
+    }
+
+    return allGearSymbols.sumOf { gearSymbol ->
+        val twoCandidates = candidates.filter { candidate -> gearSymbol in candidate.symbolPos }
+        if (twoCandidates.size == 2) {
+            val (a, b) = twoCandidates
+            a.num * b.num
+        } else if (twoCandidates.size > 2) {
+            error("WTF $twoCandidates")
+        } else 0
     }
 }
