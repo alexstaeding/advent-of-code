6725f37321ca26ee4b4c536df9b9ea26693e719d
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-04 07:17:54 +0100

    Add 2023/Day4 part b

--- years/2023/src/main/kotlin/aoc/Day4.kt
+++ years/2023/src/main/kotlin/aoc/Day4.kt
@@ -1,19 +1,36 @@
-import aoc.Framework
-import aoc.lineSequence
+package aoc
+
 import java.math.BigInteger
 
 fun main() {
-    Framework.getInput(4, useExample = false).lineSequence().day4a().let { println(it) }
+    Framework.getInput(4, useExample = false).readText().day4b().let { println(it) }
 }
 
-fun Sequence<String>.day4a(): BigInteger = withIndex().sumOf { (i, l) ->
+fun Sequence<String>.day4a(): BigInteger = sumOf { l ->
     val (left, right) = l.substringAfter(": ").split(" | ")
     val winning = "(\\d+)".toRegex().findAll(left).map { BigInteger(it.value) }.toSet()
-    val have = "(\\d+)".toRegex().findAll(right).map { BigInteger(it.value) }
-    val winCount = winning.intersect(have.toSet()).count()
+    val have = "(\\d+)".toRegex().findAll(right).map { BigInteger(it.value) }.toSet()
+    val winCount = winning.intersect(have).count()
     if (winCount < 1) {
         BigInteger.ZERO
     } else {
         BigInteger.TWO.pow(winCount - 1)
     }
+}
+
+fun String.day4b(): Int {
+    val winMap = mutableMapOf<Int, Int>()
+    val lines = lines()
+    lines.withIndex().forEach { (index, line) ->
+        val (left, right) = line.substringAfter(": ").split(" | ")
+        val winning = "(\\d+)".toRegex().findAll(left).map { BigInteger(it.value) }.toSet()
+        val have = "(\\d+)".toRegex().findAll(right).map { BigInteger(it.value) }.toSet()
+        val matchCount = winning.intersect(have).count()
+        repeat((winMap[index] ?: 0) + 1) {
+            for (i in (index + 1)..matchCount + index) {
+                winMap[i] = winMap.getOrDefault(i, 0) + 1
+            }
+        }
+    }
+    return winMap.values.sum() + lines.size
 }
