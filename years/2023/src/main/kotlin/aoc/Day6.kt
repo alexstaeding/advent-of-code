a1c8e877c8b6d55992ca78451c2dfd2da9e3409f
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-06 06:28:33 +0100

    Shorten 2023/Day6

--- years/2023/src/main/kotlin/aoc/Day6.kt
+++ years/2023/src/main/kotlin/aoc/Day6.kt
@@ -7,20 +7,18 @@ fun main() {
 }
 
 fun BufferedReader.day6a(): Int {
-    val times = "(\\d+)".toRegex().findAll(readLine().split(":")[1]).map { it.value.toInt() }
-    val distances = "(\\d+)".toRegex().findAll(readLine().split(":")[1]).map { it.value.toInt() }
-    return times.zip(distances).map { (time, distanceRecord) ->
+    val (times, records) = List(2) { "(\\d+)".toRegex().findAll(readLine().split(":")[1]).map { it.value.toInt() } }
+    return times.zip(records).map { (time, record) ->
         // for each speed
         (0..time)
             .map { t -> (time - t) * t }
-            .count { it > distanceRecord }
+            .count { it > record }
     }.reduce { a, b -> a * b }
 }
 
 fun BufferedReader.day6b(): Int {
-    val time = readLine().split(":")[1].replace("\\s+".toRegex(), "").toLong()
-    val distanceRecord = readLine().split(":")[1].replace("\\s+".toRegex(), "").toLong()
+    val (time, record) = List(2) { readLine().split(":")[1].replace("\\s+".toRegex(), "").toLong() }
     return (0..time)
         .map { t -> (time - t) * t }
-        .count { it > distanceRecord }
+        .count { it > record }
 }
