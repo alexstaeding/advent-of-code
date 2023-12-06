e451868192d9d864c537f4495c939667e4fdf460
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-06 16:56:51 +0100

    2023/Day5 part b example works, but not real input

--- years/2023/src/main/kotlin/aoc/Day5.kt
+++ years/2023/src/main/kotlin/aoc/Day5.kt
@@ -1,24 +1,27 @@
 package aoc
 
-fun main() = Framework.getInput(5, useExample = false).readLines().day5a().let { println(it) }
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() = Framework.getInput(5, useExample = false).readLines().day5b().let { println(it) }
 
 private data class Mapping(
     val dest: Long,
     val source: Long,
     val count: Long,
-)
+) {
+    val destRange = dest..<dest + count
+    val sourceRange = source..<source + count
+    val destDelta = dest - source
+
+    override fun toString(): String = "$dest $source $count"
+}
+
+fun LongRange.intersects(other: LongRange): Boolean = this.first <= other.last && other.first <= this.last
 
 private fun String.toMapping(): Mapping {
     val (dest, source, count) = split(" ").map { it.toLong() }
     return Mapping(dest, source, count)
-}
-
-private fun processMappings(nums: List<Long>, mappings: List<Mapping>): List<Long> {
-    return nums.map { n ->
-        mappings.firstOrNull { (_, s, c) -> n in s..s + c }?.let { mapping ->
-            (n - mapping.source) + mapping.dest
-        } ?: n
-    }
 }
 
 fun List<String>.day5a(): Long {
@@ -27,5 +30,49 @@ fun List<String>.day5a(): Long {
         .map { it.substringAfter("\n") }
         .map { it.split("\n").map { l -> l.toMapping() } }
 
-    return parts.fold(seeds) { acc, mappings -> processMappings(acc, mappings) }.min()
+    return parts.fold(seeds) { nums, mappings ->
+        nums.map { n ->
+            mappings.firstOrNull { (_, s, c) -> n in s..s + c }?.let { mapping ->
+                (n - mapping.source) + mapping.dest
+            } ?: n
+        }
+    }.min()
+}
+
+private fun LongRange.mapWith(mappings: List<Mapping>): List<LongRange> {
+    return mappings.filter { it.sourceRange.intersects(this) }.fold(listOf(this)) { acc, mapping ->
+        acc.flatMap {
+            buildList {
+                // before source range
+                if (it.first < mapping.sourceRange.first) {
+                    add(it.first..min(it.last, mapping.sourceRange.first - 1))
+                }
+                // in source range
+                if (it.intersects(mapping.sourceRange)) {
+                    add(max(it.first + mapping.destDelta, mapping.dest)..min(it.last + mapping.destDelta, mapping.destRange.last))
+                }
+                // after source range
+                if (it.last > mapping.sourceRange.last) {
+                    add(max(it.first, mapping.sourceRange.last + 1)..it.last)
+                }
+            }
+        }
+    }
+}
+
+fun List<String>.day5b(): Long {
+    val seeds = "(\\d+) (\\d+)".toRegex().findAll(this[0].split(": ")[1])
+        .map { it.destructured }
+        .map { (a, b) -> a.toLong().let { x -> x..x + b.toLong() } }
+        .toList()
+
+    val parts = asSequence().drop(2).joinToString("\n").split("\n\n")
+        .map { it.substringAfter("\n") }
+        .map { it.split("\n").map { l -> l.toMapping() } }
+
+    return parts.foldIndexed(seeds) { i, nums, mappings ->
+        nums.flatMap { n ->
+            n.mapWith(mappings)
+        }
+    }.minOf { it.first }
 }
