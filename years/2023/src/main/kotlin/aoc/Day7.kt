b51f68e99e9bb73a88504e7a394185636bbf4890
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-07 14:48:34 +0100

    Add tests for 2023/Day7 and clean up slightly

--- years/2023/src/main/kotlin/aoc/Day7.kt
+++ years/2023/src/main/kotlin/aoc/Day7.kt
@@ -1,6 +1,6 @@
 package aoc
 
-sealed interface Kind {
+fun interface Kind {
     fun matches(s: String): Boolean
 
     data object FiveOfAKind : Kind {
@@ -41,66 +41,14 @@ sealed interface Kind {
     }
 }
 
-fun jokerDelegate(s: String, kind: Kind): Boolean = cards.keys.any { j ->
-    kind.matches(s.replace('J', j))
-}
-
-sealed interface JokerKind : Kind {
-
-    data object FiveOfAKind : JokerKind {
-        override fun matches(s: String): Boolean = jokerDelegate(s, Kind.FiveOfAKind)
-    }
-
-    data object FourOfAKind : JokerKind {
-        override fun matches(s: String): Boolean = jokerDelegate(s, Kind.FourOfAKind)
-    }
-
-    data object FullHouse : JokerKind {
-        override fun matches(s: String): Boolean = jokerDelegate(s, Kind.FullHouse)
-    }
-
-    data object ThreeOfAKind : JokerKind {
-        override fun matches(s: String): Boolean = jokerDelegate(s, Kind.ThreeOfAKind)
-    }
-
-    data object TwoPair : JokerKind {
-        override fun matches(s: String): Boolean = jokerDelegate(s, Kind.TwoPair)
-    }
-
-    data object OnePair : JokerKind {
-        override fun matches(s: String): Boolean = jokerDelegate(s, Kind.OnePair)
-    }
-
-    data object HighCard : JokerKind {
-        override fun matches(s: String): Boolean = true
-    }
-}
-
 val cards = listOf('A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2').reversed()
     .withIndex().associate { (i, v) -> v to i }
 val cardsWithJoker = listOf('A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J').reversed()
     .withIndex().associate { (i, v) -> v to i }
-val hands =
-    listOf(
-        Kind.HighCard,
-        Kind.OnePair,
-        Kind.TwoPair,
-        Kind.ThreeOfAKind,
-        Kind.FullHouse,
-        Kind.FourOfAKind,
-        Kind.FiveOfAKind,
-    ).withIndex().associate { (i, v) -> v to i }
+val hands = listOf(Kind.HighCard, Kind.OnePair, Kind.TwoPair, Kind.ThreeOfAKind, Kind.FullHouse, Kind.FourOfAKind, Kind.FiveOfAKind)
+    .withIndex().associate { (i, v) -> v to i }
 
-val handsWithJoker =
-    listOf(
-        JokerKind.HighCard,
-        JokerKind.OnePair,
-        JokerKind.TwoPair,
-        JokerKind.ThreeOfAKind,
-        JokerKind.FullHouse,
-        JokerKind.FourOfAKind,
-        JokerKind.FiveOfAKind,
-    ).withIndex().associate { (i, v) -> v to i }
+val handsWithJoker = hands.mapKeys { (k, _) -> Kind { cards.keys.any { j -> k.matches(it.replace('J', j)) } } }
 
 fun handComparator(hands: Map<out Kind, Int>, cards: Map<Char, Int>): Comparator<String> = Comparator.comparing<String, Int> {
     hands[it.getHandKind(hands)] ?: -1
@@ -109,15 +57,7 @@ fun handComparator(hands: Map<out Kind, Int>, cards: Map<Char, Int>): Comparator
         ?: 0
 }
 
-fun String.getHandKind(hands: Map<out Kind, Int>): Kind {
-    val result = hands.filter { it.key.matches(this) }.maxBy { it.value }.key
-    println("Hand $this -> ${result::class.simpleName}")
-    return result
-}
-
-fun main() {
-    Framework.getInput(7, useExample = false).lineSequence().day7b().let { println(it) }
-}
+fun String.getHandKind(hands: Map<out Kind, Int>): Kind = hands.filter { it.key.matches(this) }.maxBy { it.value }.key
 
 private fun Sequence<String>.day7(hands: Map<out Kind, Int>, cards: Map<Char, Int>): Int = map { it.split(" ") }
     .associate { (hand, bid) -> hand to bid.toInt() }
---
+++ years/2023/src/test/kotlin/aoc/Day7Test.kt
@@ -0,0 +1,21 @@
+package aoc
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+private val spec: FunSpec.() -> Unit = {
+    test("inputFileExampleA") {
+        Framework.getInput(7, useExample = true).lineSequence().day7a() shouldBe 6440
+    }
+    test("inputFileExampleB") {
+        Framework.getInput(7, useExample = true).lineSequence().day7b() shouldBe 5905
+    }
+    test("inputFileA") {
+        Framework.getInput(7).getReader().lineSequence().day7a() shouldBe 246424613
+    }
+    test("inputFileB") {
+        Framework.getInput(7).getReader().lineSequence().day7b() shouldBe 248256639
+    }
+}
+
+class Day7Test : FunSpec(spec)
