b5f2dfef3d682af552be635b485b7458f112bf35
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-07 15:22:10 +0100

    Clean up 2023/Day5 even super more

--- years/2023/src/main/kotlin/aoc/Day7.kt
+++ years/2023/src/main/kotlin/aoc/Day7.kt
@@ -1,39 +1,31 @@
 package aoc
 
-fun interface Kind {
-    fun matches(s: String): Boolean
-}
+private typealias KindFilter = (String) -> Boolean
 
-val cards = setOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
-    .withIndex().associate { (i, v) -> v to i }
-val cardsWithJoker = (setOf('J') + cards.keys)
-    .withIndex().associate { (i, v) -> v to i }
+private val cards = setOf('2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A')
 
-val hands = listOf(
-    Kind { true },
-    Kind { s -> s.toSet().any { c -> s.count { c == it } == 2 } },
-    Kind { s -> s.toSet().count { c -> s.count { c == it } == 2 } == 2 },
-    Kind { s -> s.toSet().any { c -> s.count { c == it } == 3 } },
-    Kind { s -> s.toSet().let { x -> x.any { c -> s.count { c == it } == 3 } && x.any { c -> s.count { c == it } == 2 } } },
-    Kind { s -> s.toSet().any { c -> s.count { c == it } == 4 } },
-    Kind { s -> s.toSet().all { it == s[0] } },
-).withIndex().associate { (i, v) -> v to i }
+private val hands: List<KindFilter> = listOf(
+    { true },
+    { s -> s.toSet().any { c -> s.count { c == it } == 2 } },
+    { s -> s.toSet().count { c -> s.count { c == it } == 2 } == 2 },
+    { s -> s.toSet().any { c -> s.count { c == it } == 3 } },
+    { s -> s.toSet().let { x -> x.any { c -> s.count { c == it } == 3 } && x.any { c -> s.count { c == it } == 2 } } },
+    { s -> s.toSet().any { c -> s.count { c == it } == 4 } },
+    { s -> s.toSet().all { it == s[0] } },
+)
 
-val handsWithJoker = hands.mapKeys { (k, _) -> Kind { cards.keys.any { j -> k.matches(it.replace('J', j)) } } }
-
-fun <V> compareHands(hands: Map<out Kind, Int>, cards: Map<Char, Int>): Comparator<Pair<String, V>> =
-    Comparator.comparing<Pair<String, V>, Int> { (s, _) ->
-        hands.filter { it.key.matches(s) }.maxOf { it.value }
-    }.then { (a, _), (b, _) ->
-        (0..<5).firstNotNullOfOrNull { (cards[a[it]] ?: -1).compareTo(cards[b[it]] ?: -1).takeIf { x -> x != 0 } } ?: 0
-    }
-
-private fun Sequence<String>.day7(hands: Map<out Kind, Int>, cards: Map<Char, Int>): Int = map { it.split(" ") }
+private fun Sequence<String>.day7(hands: List<KindFilter>, cards: Set<Char>): Int = map { it.split(" ") }
     .map { (hand, bid) -> hand to bid.toInt() }
-    .sortedWith(compareHands(hands, cards))
+    .sortedWith(
+        Comparator.comparing<Pair<String, Int>, Int> { (s, _) ->
+            hands.withIndex().filter { (_, k) -> k(s) }.maxOf { it.index }
+        }.then { (a, _), (b, _) ->
+            (0..<5).firstNotNullOfOrNull { i -> cards.indexOf(a[i]).compareTo(cards.indexOf(b[i])).takeIf { it != 0 } } ?: 0
+        },
+    )
     .map { (_, v) -> v }
     .withIndex()
     .sumOf { (i, v) -> (i + 1) * v }
 
 fun Sequence<String>.day7a() = day7(hands, cards)
-fun Sequence<String>.day7b() = day7(handsWithJoker, cardsWithJoker)
+fun Sequence<String>.day7b() = day7(hands.map { k -> { cards.any { j -> k(it.replace('J', j)) } } }, setOf('J') + cards)
