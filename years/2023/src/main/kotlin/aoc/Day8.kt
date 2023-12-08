d095ed37a14c726f1c3e4f75c18c8224b82a3021
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-08 13:22:40 +0100

    Add 2023/Day8 part b

--- years/2023/src/main/kotlin/aoc/Day8.kt
+++ years/2023/src/main/kotlin/aoc/Day8.kt
@@ -3,23 +3,23 @@ package aoc
 import java.io.BufferedReader
 
 fun main() {
-    Framework.getInput(8, "a", useExample = true).getReader().day8a().let { println(it) }
+    Framework.getInput(8, useExample = false).getReader().day8b().let { println(it) }
 }
 
-val regex = Regex("([A-Z]+) = \\(([A-Z]+), ([A-Z]+)\\)")
+val regex = Regex("([1-9A-Z]+) = \\(([1-9A-Z]+), ([1-9A-Z]+)\\)")
 
-fun BufferedReader.day8a(): Int {
-    val instructionLine = readLine().toList()
+private fun solve(
+    start: String,
+    map: Map<String, Pair<String, String>>,
+    instructionLine: List<Char>,
+    terminateWhen: (String) -> Boolean,
+): ULong {
     val instructions = sequence {
         while (true) yieldAll(instructionLine)
     }.iterator()
-    readLine()
-    val map = readLines()
-        .map { regex.findAll(it).single().destructured }
-        .associate { (id, l, r) -> id to (l to r) }
-    var current = "AAA"
-    var i = 0
-    while (current != "ZZZ") {
+    var current = start
+    var i = 0UL
+    while (!terminateWhen(current)) {
         i++
         current = map[current]!!.let { (l, r) ->
             if (instructions.next() == 'L') {
@@ -29,3 +29,26 @@ fun BufferedReader.day8a(): Int {
     }
     return i
 }
+
+fun BufferedReader.day8a(): ULong {
+    val instructionLine = readLine().toList()
+    readLine()
+    val map = readLines()
+        .map { regex.findAll(it).single().destructured }
+        .associate { (id, l, r) -> id to (l to r) }
+    return solve("AAA", map, instructionLine) { it == "ZZZ" }
+}
+
+fun BufferedReader.day8b(): ULong {
+    val instructionLine = readLine().toList()
+    readLine()
+    val map = readLines()
+        .map { regex.findAll(it).single().destructured }
+        .associate { (id, l, r) -> id to (l to r) }
+    return map.keys.filter { it.endsWith('A') }.map { start ->
+        solve(start, map, instructionLine) { it.endsWith('Z') }
+    }.reduce { a, b -> lcm(a, b) }
+}
+
+private fun gcd(a: ULong, b: ULong): ULong = if (b == 0UL) a else gcd(b, a % b)
+private fun lcm(a: ULong, b: ULong): ULong = a / gcd(a, b) * b
