64fdeaafa921dd535f7e691231b0fc06bf9efa42
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-09 06:34:03 +0100

    Add 2023/Day9 part b

--- years/2023/src/main/kotlin/aoc/Day9.kt
+++ years/2023/src/main/kotlin/aoc/Day9.kt
@@ -1,7 +1,7 @@
 package aoc
 
 fun main() {
-    Framework.getInput(9, useExample = false).lineSequence().day9a().let { println(it) }
+    Framework.getInput(9, useExample = false).lineSequence().day9b().let { println(it) }
 }
 
 fun Sequence<String>.day9a(): Long = sumOf { s ->
@@ -16,10 +16,30 @@ fun Sequence<String>.day9a(): Long = sumOf { s ->
     }
 
     var lastDiff = 0L
-    for ((i, line) in lines.reversed().withIndex()) {
+    for (line in lines.reversed()) {
         line += line.last() + lastDiff
         lastDiff = line.last()
     }
 
     lines.first().last()
+}
+
+fun Sequence<String>.day9b(): Long = sumOf { s ->
+    val firstLine = Regex("(-?\\d+)").findAll(s).map { it.value.toLong() }
+        .toMutableList()
+
+    val lines = mutableListOf(firstLine)
+
+    while (!lines.last().all { it == 0L }) {
+        lines += lines.last().windowed(2, 1) { (l, r) -> r - l }
+            .toMutableList()
+    }
+
+    var lastDiff = 0L
+    for (line in lines.reversed()) {
+        line.add(0, line.first() - lastDiff)
+        lastDiff = line.first()
+    }
+
+    lines.first().first()
 }
