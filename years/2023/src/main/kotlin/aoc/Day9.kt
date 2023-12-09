882046254595013a76fca17c542b9a10e31c9947
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-09 06:37:02 +0100

    Clean up 2023/Day9

--- years/2023/src/main/kotlin/aoc/Day9.kt
+++ years/2023/src/main/kotlin/aoc/Day9.kt
@@ -1,20 +1,16 @@
 package aoc
 
-fun main() {
-    Framework.getInput(9, useExample = false).lineSequence().day9b().let { println(it) }
-}
-
-fun Sequence<String>.day9a(): Long = sumOf { s ->
-    val firstLine = Regex("(-?\\d+)").findAll(s).map { it.value.toLong() }
-        .toMutableList()
-
-    val lines = mutableListOf(firstLine)
-
+private fun String.calculateLines(): MutableList<MutableList<Long>> {
+    val lines = mutableListOf(Regex("(-?\\d+)").findAll(this).map { it.value.toLong() }.toMutableList())
     while (!lines.last().all { it == 0L }) {
         lines += lines.last().windowed(2, 1) { (l, r) -> r - l }
             .toMutableList()
     }
+    return lines
+}
 
+fun Sequence<String>.day9a(): Long = sumOf { s ->
+    val lines = s.calculateLines()
     var lastDiff = 0L
     for (line in lines.reversed()) {
         line += line.last() + lastDiff
@@ -25,16 +21,7 @@ fun Sequence<String>.day9a(): Long = sumOf { s ->
 }
 
 fun Sequence<String>.day9b(): Long = sumOf { s ->
-    val firstLine = Regex("(-?\\d+)").findAll(s).map { it.value.toLong() }
-        .toMutableList()
-
-    val lines = mutableListOf(firstLine)
-
-    while (!lines.last().all { it == 0L }) {
-        lines += lines.last().windowed(2, 1) { (l, r) -> r - l }
-            .toMutableList()
-    }
-
+    val lines = s.calculateLines()
     var lastDiff = 0L
     for (line in lines.reversed()) {
         line.add(0, line.first() - lastDiff)
