3430ee8c0af01098fa34c3cdecb8bc9090fd773a
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-09 06:49:25 +0100

    Add tests for 2023/Day9

---
+++ years/2023/src/test/kotlin/aoc/Day9Test.kt
@@ -0,0 +1,21 @@
+package aoc
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+private val spec: FunSpec.() -> Unit = {
+    test("inputFileExampleA") {
+        Framework.getInput(9,  useExample = true).lineSequence().day9a() shouldBe 114L
+    }
+    test("inputFileExampleB") {
+        Framework.getInput(9, useExample = true).lineSequence().day9b() shouldBe 2L
+    }
+    test("inputFileA") {
+        Framework.getInput(9).lineSequence().day9a() shouldBe 1702218515L
+    }
+    test("inputFileB") {
+        Framework.getInput(9).lineSequence().day9b() shouldBe 925L
+    }
+}
+
+class Day9Test : FunSpec(spec)
