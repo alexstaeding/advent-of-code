7b24ce27428f1e101d73b224996be32b85856aeb
Alexander Städing <alexanderstaeding@gmail.com>
2023-12-16 06:37:54 +0100

    Add 2023/Day16 part b

--- years/2023/src/main/kotlin/aoc/Day16.kt
+++ years/2023/src/main/kotlin/aoc/Day16.kt
@@ -22,7 +22,7 @@ private operator fun Grid16.get(pos: Pos16) = this[pos.y][pos.x]
 private operator fun Grid16.contains(pos: Pos16) = pos.y in indices && pos.x in this[pos.y].indices
 
 fun main() {
-    Framework.getInput(16, useExample = false).readText().day16a().let { println(it) }
+    Framework.getInput(16, useExample = false).readText().day16b().let { println(it) }
 }
 
 private fun Grid16.println(visited: Set<Pos16>) {
@@ -38,20 +38,19 @@ private fun Grid16.println(visited: Set<Pos16>) {
     }
 }
 
-fun String.day16a(): Int {
-    val grid = lines().map { it.toList() }
+private fun Grid16.doThing(start: BeamPos): Int {
     val queue = ArrayDeque<BeamPos>()
-    queue.add(BeamPos(Pos16(0, 0), Dir16.RIGHT))
+    queue.add(start)
     val visited = mutableSetOf<BeamPos>()
 
     while (queue.isNotEmpty()) {
         val current = queue.removeFirst()
-        if (current.pos !in grid || current in visited) {
+        if (current.pos !in this || current in visited) {
             continue
         }
 
         visited.add(current)
-        when (grid[current.pos]) {
+        when (this[current.pos]) {
             '|' -> {
                 when (current.dir) {
                     Dir16.UP, Dir16.DOWN -> queue.add(BeamPos(current.pos + current.dir.pos, current.dir))
@@ -92,4 +91,21 @@ fun String.day16a(): Int {
         }
     }
     return visited.mapTo(mutableSetOf()) { it.pos }.size
+}
+
+private fun String.day16a(): Int {
+    val grid = lines().map { it.toList() }
+    return grid.doThing(BeamPos(Pos16(0, 0), Dir16.RIGHT))
+}
+
+private fun String.day16b(): Int {
+    val grid = lines().map { it.toList() }
+
+    val top = List(grid[0].size) { Pos16(0, it) }.map { BeamPos(it, Dir16.DOWN) }
+    val bottom = List(grid[0].size) { Pos16(grid.size - 1, it) }.map { BeamPos(it, Dir16.UP) }
+    val left = List(grid.size) { Pos16(it, 0) }.map { BeamPos(it, Dir16.RIGHT) }
+    val right = List(grid.size) { Pos16(it, grid[0].size - 1) }.map { BeamPos(it, Dir16.LEFT) }
+
+    val all = top + bottom + left + right
+    return all.maxOf { grid.doThing(it) }
 }
