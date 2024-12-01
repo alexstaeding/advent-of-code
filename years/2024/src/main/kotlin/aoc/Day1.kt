a17ac209cc1b6829548d8b1e0d6452e52955071e
Alexander Städing <alexanderstaeding@gmail.com>
2024-12-01 08:44:20 +0100

    Add 2024/Day1 part b

--- years/2024/src/main/kotlin/aoc/Day1.kt
+++ years/2024/src/main/kotlin/aoc/Day1.kt
@@ -4,17 +4,31 @@ import kotlin.math.abs
 
 private val numPairRegex = """(\d+)\s+(\d+)""".toRegex()
 
-fun Sequence<String>.day1a(): Int {
-    val all = toList().map {
+private fun Sequence<String>.parseInput(): List<Pair<Int, Int>> =
+    toList().map {
         (numPairRegex.matchEntire(it)
             ?: throw IllegalArgumentException("Invalid input format: $it"))
             .groupValues
     }.map { (_, a, b) -> a.toInt() to b.toInt() }
+
+fun Sequence<String>.day1a(): Int {
+    val all = parseInput()
 
     val left = all.map { it.first }.sorted()
     val right = all.map { it.second }.sorted()
 
     return left.zip(right).sumOf { (l, r) ->
         abs(l - r)
+    }
+}
+
+fun Sequence<String>.day1b(): Int {
+    val all = parseInput()
+
+    val left = all.map { it.first }
+    val right = all.map { it.second }.groupingBy { it }.eachCount()
+
+    return left.sumOf {
+        it * (right[it] ?: 0)
     }
 }
--- years/2024/src/test/kotlin/aoc/Day1Test.kt
+++ years/2024/src/test/kotlin/aoc/Day1Test.kt
@@ -4,11 +4,17 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
 private val spec: FunSpec.() -> Unit = {
-    test("inputFileExample") {
+    test("inputFileExampleA") {
         Framework.getInput(1, useExample = true).lineSequence().day1a() shouldBe 11
     }
-    test("inputFile") {
+    test("inputFileExampleA") {
+        Framework.getInput(1, useExample = true).lineSequence().day1b() shouldBe 31
+    }
+    test("inputFileA") {
         Framework.getInput(1, useExample = false).lineSequence().day1a() shouldBe 1722302
+    }
+    test("inputFileB") {
+        Framework.getInput(1, useExample = false).lineSequence().day1b() shouldBe 1722302
     }
 }
 
