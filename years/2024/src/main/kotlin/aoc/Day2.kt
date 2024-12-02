dff7631ed34f969fdde5553a839a71ce8b9d9914
Alexander Städing <alexanderstaeding@gmail.com>
2024-12-02 10:06:16 +0100

    Add 2024/Day2 part b

--- years/2024/src/main/kotlin/aoc/Day2.kt
+++ years/2024/src/main/kotlin/aoc/Day2.kt
@@ -1,19 +1,37 @@
 package aoc
 
-fun Sequence<String>.day2a(): Int {
-    return count { line ->
+private fun checkValid(nums: List<Int>): Boolean {
+    val decreasing = (nums[0] - nums[1]) > 0
+
+    return nums.windowed(2)
+        .all { (a, b) ->
+            if (decreasing) {
+                (a - b >= 1) && (a - b <= 3)
+            } else {
+                (b - a >= 1) && (b - a <= 3)
+            }
+        }
+}
+
+fun Sequence<String>.day2a(): Int =
+    count { line ->
         val nums = line.split(" ")
             .map { it.toInt() }
 
-        val decreasing = (nums[0] - nums[1]) > 0
-
-        nums.windowed(2)
-            .all { (a, b) ->
-                if (decreasing) {
-                    (a - b >= 1) && (a - b <= 3)
-                } else {
-                    (b - a >= 1) && (b - a <= 3)
-                }
-            }
+        checkValid(nums)
     }
-}
+
+fun Sequence<String>.day2b(): Int =
+    count { line ->
+        val nums = line.split(" ")
+            .map { it.toInt() }
+
+        nums.indices.map {
+            // remove index it from nums
+            val newNums = nums.toMutableList()
+            newNums.removeAt(it)
+            newNums
+        }.any {
+            checkValid(it)
+        }
+    }
--- years/2024/src/test/kotlin/aoc/Day2Test.kt
+++ years/2024/src/test/kotlin/aoc/Day2Test.kt
@@ -7,8 +7,14 @@ private val spec: FunSpec.() -> Unit = {
     test("inputFileExampleA") {
         Framework.getInput(2, useExample = true).lineSequence().day2a() shouldBe 2
     }
+    test("inputFileExampleB") {
+        Framework.getInput(2, useExample = true).lineSequence().day2b() shouldBe 4
+    }
     test("inputFileA") {
-        Framework.getInput(2, useExample = false).lineSequence().day2a() shouldBe 2
+        Framework.getInput(2, useExample = false).lineSequence().day2a() shouldBe 383
+    }
+    test("inputFileB") {
+        Framework.getInput(2, useExample = false).lineSequence().day2b() shouldBe 436
     }
 }
 
