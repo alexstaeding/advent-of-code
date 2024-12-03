a691178515b73ae5d2ea309af61d5615446a5b93
Alexander Städing <alexanderstaeding@gmail.com>
2024-12-03 23:36:46 +0100

    Clean up 2024/Day3

--- years/2024/src/main/kotlin/aoc/Day3.kt
+++ years/2024/src/main/kotlin/aoc/Day3.kt
@@ -2,20 +2,12 @@ package aoc
 
 private val reg = """mul\((\d+),(\d+)\)""".toRegex()
 
-private fun String.day3a(): Int =
+fun String.day3a(): Int =
     reg.findAll(this).sumOf {
         val (_, a, b) = it.groupValues
         a.toInt() * b.toInt()
     }
 
-private fun String.day3b(): Int = split("do()")
+fun String.day3b(): Int = split("do()")
     .map { it.substringBefore("don't()") }
-    .sumOf { sec ->
-        reg.findAll(sec).sumOf {
-            val (_, a, b) = it.groupValues
-            a.toInt() * b.toInt()
-        }
-    }
-
-fun Sequence<String>.day3a(): Int = sumOf { it.day3a() }
-fun Sequence<String>.day3b(): Int = joinToString("").day3b()
+    .sumOf { it.day3a() }
--- years/2024/src/test/kotlin/aoc/Day3Test.kt
+++ years/2024/src/test/kotlin/aoc/Day3Test.kt
@@ -5,16 +5,16 @@ import io.kotest.matchers.shouldBe
 
 private val spec: FunSpec.() -> Unit = {
     test("inputFileExampleA") {
-        Framework.getInput(3, "a", useExample = true).lineSequence().day3a() shouldBe 161
+        Framework.getInput(3, "a", useExample = true).readText().day3a() shouldBe 161
     }
     test("inputFileExampleB") {
-        Framework.getInput(3, "b", useExample = true).lineSequence().day3b() shouldBe 48
+        Framework.getInput(3, "b", useExample = true).readText().day3b() shouldBe 48
     }
     test("inputFileA") {
-        Framework.getInput(3, useExample = false).lineSequence().day3a() shouldBe 173731097
+        Framework.getInput(3, useExample = false).readText().day3a() shouldBe 173731097
     }
     test("inputFileB") {
-        Framework.getInput(3, useExample = false).lineSequence().day3b() shouldBe 173731097
+        Framework.getInput(3, useExample = false).readText().day3b() shouldBe 93729253
     }
 }
 
