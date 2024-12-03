4e21e8acfc2b7dc4a458eed679e0c3f89a8fecd7
Alexander Städing <alexanderstaeding@gmail.com>
2024-12-03 23:19:00 +0100

    Add 2024/Day3 part b

--- years/2024/src/main/kotlin/aoc/Day3.kt
+++ years/2024/src/main/kotlin/aoc/Day3.kt
@@ -8,4 +8,14 @@ private fun String.day3a(): Int =
         a.toInt() * b.toInt()
     }
 
+private fun String.day3b(): Int = split("do()")
+    .map { it.substringBefore("don't()") }
+    .sumOf { sec ->
+        reg.findAll(sec).sumOf {
+            val (_, a, b) = it.groupValues
+            a.toInt() * b.toInt()
+        }
+    }
+
 fun Sequence<String>.day3a(): Int = sumOf { it.day3a() }
+fun Sequence<String>.day3b(): Int = joinToString("").day3b()
